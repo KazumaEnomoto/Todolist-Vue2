d38061bd3ef57386037bc244089fc837bf25f9ca
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-09-14 15:50:40 +0900

    modalのレイヤー設定

--- src/components/TodoTask.vue
+++ src/components/TodoTask.vue
@@ -147,4 +147,15 @@ ul {
     font-size: 20px;
     color: red;
 }
+
+.modal {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.5);
+}
 </style>
